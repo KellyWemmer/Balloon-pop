78927e3d9a0a0f5359bff07773053c783fe82a14
Kelly Wemmer <kellysue3@yahoo.com>
2022-05-03 18:43:11 -0600

    Added inflation rate and change button location

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 let clickCount = 0 //keep track of number of clicks
+let height=120 //set height,width etc to int
+let width=100
+let inflationRate=20
+let maxsize= 300
 
 function inflate(){
     clickCount ++
     var balloonElement = document.getElementById("balloon")//gets div element id 'balloon'
-    balloonElement.style.height = "400px"
-    balloonElement.style.width = "300px"
+    height += inflationRate
+    width += inflationRate
+    if(height >= maxsize){
+        console.log("pop the balloon")
+        height = 0
+        width = 0        
+    }
+    balloonElement.style.height = height + "px"
+    balloonElement.style.width = width +"px"
+
+    
 
     var clickCountElem = document.getElementById("click-count")
     clickCountElem.innerText = clickCount.toString()
