57527fff1a338f1674db5eccf0f15974f47b41d3
Kelly Wemmer <kellysue3@yahoo.com>
2022-05-04 18:08:47 -0600

    Added popcount

--- main.js
+++ main.js
@@ -2,23 +2,27 @@ let clickCount = 0 //keep track of number of clicks
 let height=120 //set height,width etc to int
 let width=100
 let inflationRate=20
-let maxsize= 300
+let maxSize= 300
+let popCount = 0
 
 function inflate(){
     clickCount ++
-    var balloonElement = document.getElementById("balloon")//gets div element id 'balloon'
+    let balloonElement = document.getElementById("balloon")//gets div element id 'balloon'
     height += inflationRate
     width += inflationRate
-    if(height >= maxsize){
+
+    if(height >= maxSize){
         console.log("pop the balloon")
+        popCount++
         height = 0
-        width = 0        
+        width = 0
+        document.getElementById("pop-count").innerText = popCount.toString()    
     }
     balloonElement.style.height = height + "px"
     balloonElement.style.width = width +"px"
 
     
 
-    var clickCountElem = document.getElementById("click-count")
+    let clickCountElem = document.getElementById("click-count")
     clickCountElem.innerText = clickCount.toString()
 }
