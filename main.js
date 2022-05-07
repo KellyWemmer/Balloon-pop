923012e6def26cf6f40548955b3b974b9a504760
Kelly Wemmer <kellysue3@yahoo.com>
2022-05-07 17:31:49 -0600

    Disabled buttons after code run

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let startButton = document.getElementById("start-button")
+let inflateButton = document.getElementById("inflate-button")
+
 let clickCount = 0 //keep track of number of clicks
 let height=120 //set height,width etc to int
 let width=100
@@ -6,8 +9,16 @@ let maxSize= 300
 let popCount = 0
 
 function startGame(){
+
+    startButton.setAttribute('disabled', "true")
+    inflateButton.removeAttribute("disabled")
+
     setTimeout(() => {
         console.log("it's been 3 seconds")
+
+        inflateButton.setAttribute('disabled', "true")//After 3 seconds, run these lines of code
+        startButton.removeAttribute("disabled")        
+
     }, 3000) //button runs for 3 seconds
 }
 
