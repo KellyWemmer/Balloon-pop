93c5e6822dcd42b660a21ea8a0ffcd8f657fba8e
Kelly Wemmer <kellysue3@yahoo.com>
2022-05-08 17:10:47 -0600

    Cleaned up code with draw function

--- main.js
+++ main.js
@@ -17,29 +17,39 @@ function startGame(){
         console.log("it's been 3 seconds")
 
         inflateButton.setAttribute('disabled', "true")//After 3 seconds, run these lines of code
-        startButton.removeAttribute("disabled")        
+        startButton.removeAttribute("disabled")  
+        
+        clickCount = 0
+        height = 120
+        width = 100
+
+        draw()
 
     }, 3000) //button runs for 3 seconds
 }
 
 function inflate(){
     clickCount ++
-    let balloonElement = document.getElementById("balloon")//gets div element id 'balloon'
     height += inflationRate
     width += inflationRate
-
+    
     if(height >= maxSize){
         console.log("pop the balloon")
         popCount++
         height = 0
         width = 0
-        document.getElementById("pop-count").innerText = popCount.toString()    
-    }
+    } 
+    draw()//Once inflate function is updated, it will draw the below items into screen
+}
+
+function draw(){
+    let balloonElement = document.getElementById("balloon")//gets div element id 'balloon'
+    let clickCountElem = document.getElementById("click-count")
+    let popCountElem = document.getElementById("pop-count")
+    
     balloonElement.style.height = height + "px"
     balloonElement.style.width = width +"px"
-
     
-
-    let clickCountElem = document.getElementById("click-count")
     clickCountElem.innerText = clickCount.toString()
+    popCountElem.innerText = popCount.toString()    
 }
